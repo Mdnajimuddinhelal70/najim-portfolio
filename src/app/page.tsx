be7c5d6fb1198e467dc7b7031b28cdd1a6e46b0e
unknown <najimuddinhelalal670@gmail.com>
2025-08-19 17:52:19 +0600

    added blogs cards and added design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Blogs from "./components/Blogs";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 
@@ -6,6 +7,7 @@ const page = () => {
     <div>
       <Hero />
       <Projects />
+      <Blogs />
     </div>
   );
 };
