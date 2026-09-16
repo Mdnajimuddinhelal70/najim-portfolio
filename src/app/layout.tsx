430132e17ab792dd5bdeb2e3faa7478315fcc2b9
unknown <najimuddinhelalal670@gmail.com>
2026-09-16 23:34:29 +0600

    fix: remove unused ts-expect-error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "./context/ThemContext";
-// @ts-expect-error Next.js handles this global CSS side-effect import.
+
 import "./globals.css";
 
 const geistSans = Geist({
